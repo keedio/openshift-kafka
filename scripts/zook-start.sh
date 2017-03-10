e3e1638e204497f6bf8bfa3fc3c9f3012111a2f7
David Santamaría <dsantamaria@keedio.com>
2017-03-10 12:50:23 +0100

    some changes

--- scripts/zook-start.sh
+++ scripts/zook-start.sh
@@ -1,13 +1,11 @@
-#!/bin/bash -e
 
 PROPERTIES=$1
 PREFIX=$2
 MYID=$3
-MAXID=$5
+MAXID=$4
 
 echo $MYID >/tmp/zookeeper/myid
-for i in 1 2 3 4 5
- do
+for ((i=1; i<=$MAXID; i++)); do
   echo server.$i=$PREFIX-$i:2888:3888
 done >>$PROPERTIES
 cat >>$PROPERTIES <<EOF
@@ -17,4 +15,4 @@ initLimit=10
 syncLimit=5
 EOF
 
- exec bin/zookeeper-server-start.sh $PROPERTIES
+exec bin/zookeeper-server-start.sh $PROPERTIES
