4522ffac5720cc264c0aba9a3a350280173b37d3
David Santamaría <dsantamaria@keedio.com>
2017-03-21 18:33:28 +0100

    some changes

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed -i 's/${BROKER_ID}/'$BROKER'/g' /opt/kafka/config/server.properties
 sed -i 's/${ZOOKEEPER}/'$ZOOKEEPER'/g' /opt/kafka/config/server.properties
-sed -i 's/${HOST_NAME}/'$HOST_NAME'/g'/opt/kafka/config/server.properties
+sed -i 's/${HOST_NAME}/'$HOST_NAME'/g' /opt/kafka/config/server.properties
 sed -i 's/${PORT}/'$PORT'/g' /opt/kafka/config/server.properties
 
 cat /opt/kafka/config/server.properties
