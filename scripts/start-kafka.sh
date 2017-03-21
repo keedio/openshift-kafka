0489346b501fb63244b383d63332f38394f3f3e7
David Santamaría <dsantamaria@keedio.com>
2017-03-21 16:40:03 +0100

    Kafka service modified

--- scripts/start-kafka.sh
+++ scripts/start-kafka.sh
@@ -12,7 +12,22 @@ zookeeper.connect=$ZOOKEEPER
 advertised.host.name=$HOST_NAME
 
 EOF
+ZOOKEEPER
+BROKER_ID
+HOST_NAME
 
-echo $PROPERTIES
+sed -i 's/${BROKER_ID}/'$BROKER'/g' $PROPERTIES 
+
+cat $PROPERTIES
 
 exec bin/kafka-server-start.sh $PROPERTIES
+
+#!/bin/bash
+
+
+
+sed -i 's/${SEEDS}/'${Array[0]}'/g' $PROPERTIES 
+
+
+
+
